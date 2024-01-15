827615da605d0b315ab4724344b81417ade07d28
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-01-15 10:37:14 +0000

    1

---
+++ play.js
@@ -0,0 +1,3 @@
+const myName = "michael";
+
+console.log(myName);
--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+import { getAllPosts } from "./utils/fetch";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    const allThePosts = await getAllPosts();
+
+    setPosts(allThePosts);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="btn-section">
+        <button onClick={(e) => handleClick(e)}>Get Posts</button>
+      </div>
+      <div className="posts-section">
+        {posts.map((post, key) => (
+          <p>{post.title}</p>
+        ))}
+      </div>
     </div>
   );
 }
